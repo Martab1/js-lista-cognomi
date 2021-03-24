7a83fae94c1373e003dfb8bfa7c617ed2612ddbd
Martab1 <martabernardo86@gmail.com>
2021-03-24 15:33:49 +0100

    the end script in console

---
+++ js/script.js
@@ -0,0 +1,31 @@
+
+
+/* 
+
+Consegna
+1. chiedi all’utente il cognome
+2. inseriscilo in un array con altri cognomi: ‘bianchi’, ‘neri’, ‘rossi’, ‘verdi’, ‘gialli’
+3. stampa la lista ordinata alfabeticamente
+4. scrivi anche la posizione "umana", 1 oppure 2 oppure 3 oppure ecc... (non 0 oppure 1 oppure 2 oppure ecc.. tipici degli array), 
+del nuovo utente inserito sopra all'interno dell'array, dopo l'ordinamento alfabetico
+
+*/
+
+
+// 1. chiedi cognome
+var cognome = prompt ("inserisci il tuo cognome");
+console.log(cognome);
+
+// 2. inserimento nell'Array
+var list = ["bianchi", "neri", "rossi", "verdi", "gialli"];
+list.push(cognome);
+
+// 3. lista ordinata alfabeticamente
+console.log(list.sort());
+
+// 4. posizione elementi da 1 
+console.log(list.indexOf(cognome) + 1);
+
+
+
+
